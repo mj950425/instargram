15d61606c406dbc87e628a9b5c7acb323798d08d
minjoon_lee <minjoon1995@naver.com>
2021-06-02 19:50:30 +0900

    [feature] 사진 업로드 정보 DB에 추가 하는 기능

--- src/main/java/com/cos/photogramstart/service/ImageService.java
+++ src/main/java/com/cos/photogramstart/service/ImageService.java
@@ -1,6 +1,7 @@
 package com.cos.photogramstart.service;
 
 import com.cos.photogramstart.config.auth.PrincipalDetails;
+import com.cos.photogramstart.domain.image.Image;
 import com.cos.photogramstart.domain.image.ImageRepository;
 import com.cos.photogramstart.web.dto.image.ImageUploadDto;
 import lombok.RequiredArgsConstructor;
@@ -31,6 +32,10 @@ public class ImageService {
             e.printStackTrace();
         }
 
+        Image image = imageUploadDto.toEntity(imageFileName,principalDetails.getUser());
+        Image imageEntity = imageRepository.save(image);
+
+        System.out.println(imageEntity);
     }
 
 }
